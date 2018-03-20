25aac2db19466d4237716bfec93d3a044d7f7dfe
hanus2 <hanus2@PC525.SPSEJECNANET.CZ>
2018-03-20 11:12:26 +0100

    xor and okno

--- src/pack/Okno.java
+++ src/pack/Okno.java
@@ -1,5 +1,26 @@
 package pack;
 
-public class Okno {
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class Okno extends JFrame {
+	private JLabel label;
+	private JTextField field;
+	private JButton but;
+	private JTextArea area;
+	
+	public Okno() {
+		this.setTitle("šifrování XOR");
+		this.setResizable(false);
+		this.setSize(700, 400);
+		this.setLayout(new FlowLayout());
+		
+		
+	}
 
 }
