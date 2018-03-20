1ea3d610ce1e92c41d3d4c5118f91bc65f74ae8a
hanus2 <hanus2@PC525.SPSEJECNANET.CZ>
2018-03-20 11:24:09 +0100

    okno

--- src/pack/Okno.java
+++ src/pack/Okno.java
@@ -19,8 +19,18 @@ public class Okno extends JFrame {
 		this.setResizable(false);
 		this.setSize(700, 400);
 		this.setLayout(new FlowLayout());
-		
-		
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		label = new JLabel("Zadejte cestu: ");
+		this.add(label);
+		field = new JTextField(40);
+		this.add(field);
+		but = new JButton("Zašifrovat");
+		this.add(but);
+		area = new JTextArea(15,45);
+		this.add(area);
+		this.setVisible(true);
 	}
+	
+	
 
 }
